6e7915360f93d14a7fce4d045aabfc7d3a4b2c68
Anutka777 <rudak0wa.nat@yandex.ru>
2020-11-04 11:25:25 +0500

    Fix prompts

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 // Container for grid
 const gridNet = document.getElementById('grid-net');
 
+// reset-button with event listener
+const resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', reset);
+
+// Make initial page layout
 let gridItemInOneLine = 16;
 makePad(gridItemInOneLine);
 drawing();
 
-// Making gird-net for drawing pad
+// Make drawing pad
 function makePad(gridItemInOneLine) {
+
+  // Use array for making grid-items
   let gridItemArray = [];
   for (i = 1; i <= (gridItemInOneLine ** 2); i++) {
     gridItemArray.push(i);
   }
-  console.log(gridItemArray);
+
+  // Change properties, based on user input (gridItemInOneLine)
   document.getElementById('grid-net').style.cssText = `grid-template-columns: repeat(${gridItemInOneLine}, 1fr);
     grid-template-rows: repeat(${gridItemInOneLine}, 1fr);`;
+
+  // Place our grid-items to document
   gridItemArray.forEach(function(el) {
     let gridItem = document.createElement('div');
     gridItem.className = 'grid-item'
@@ -22,26 +32,28 @@ function makePad(gridItemInOneLine) {
   });
 }
 
-// Reset Button
-const resetBtn = document.getElementById('reset');
-resetBtn.addEventListener('click', reset);
-
+// Click on reset-button
 function reset() {
-  while(gridNet.firstChild) {
+
+  // Remove previos grid-net
+  while (gridNet.firstChild) {
     gridNet.firstChild.remove();
   }
-  gridItemInOneLine = prompt ('How many squares in one line do you prefer? \nPlease don\'t chose number > 100');
-  if (gridItemInOneLine > 100) {
+
+  // Prompt for user input
+  gridItemInOneLine = +prompt ('How many squares in one line do you prefer? \nPlease don\'t chose number > 100');
+  if (typeof(gridItemInOneLine) === 'number' && gridItemInOneLine > 100) {
     alert ("Please limimt yourself with numbers <= 100");
     reset();
-  } else {
+  } else if (typeof(gridItemInOneLine) === 'number' && gridItemInOneLine <= 100) {
     makePad(gridItemInOneLine);
     drawing();
+  } else {
+    alert("Only numbers accepted. Please click Erase button again.");
   }
 }
 
-
-// Drawing by mouse move
+// Change class of grid-item by hovering
 function drawing() {
   Array.from(document.getElementsByClassName('grid-item')).forEach(function(item) {
     item.addEventListener('mouseenter', function(e) {
