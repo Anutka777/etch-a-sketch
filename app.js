40550814e50bfc97f476c8d99577daa1345e6bf4
Anutka777 <rudak0wa.nat@yandex.ru>
2020-11-04 10:54:40 +0500

    Add Erase button with prototype functionality

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
+// Container for grid
 const gridNet = document.getElementById('grid-net');
 
-let gridItemArray = [];
-let gridItemCount = 256;
-for (i = 1; i <= gridItemCount; i++) {
-  gridItemArray.push(i);
+let gridItemInOneLine = 16;
+makePad(gridItemInOneLine);
+drawing();
+
+// Making gird-net for drawing pad
+function makePad(gridItemInOneLine) {
+  let gridItemArray = [];
+  for (i = 1; i <= (gridItemInOneLine ** 2); i++) {
+    gridItemArray.push(i);
+  }
+  console.log(gridItemArray);
+  document.getElementById('grid-net').style.cssText = `grid-template-columns: repeat(${gridItemInOneLine}, 1fr);
+    grid-template-rows: repeat(${gridItemInOneLine}, 1fr);`;
+  gridItemArray.forEach(function(el) {
+    let gridItem = document.createElement('div');
+    gridItem.className = 'grid-item'
+    gridItem.id = `grid-item-${el}`;
+    gridNet.appendChild(gridItem);
+  });
 }
 
-gridItemArray.forEach(function(el) {
-  let gridItem = document.createElement('div');
-  gridItem.className = 'grid-item'
-  gridItem.id = `grid-item-${el}`;
-  gridNet.appendChild(gridItem);
-  });
+// Reset Button
+const resetBtn = document.getElementById('reset');
+resetBtn.addEventListener('click', reset);
 
-Array.from(document.getElementsByClassName('grid-item')).forEach(function(item) {
-  item.addEventListener('mouseenter', function(e) {
-    e.target.classList.add('drawing');
+function reset() {
+  while(gridNet.firstChild) {
+    gridNet.firstChild.remove();
+  }
+  gridItemInOneLine = prompt ('How many squares in one line do you prefer? \nPlease don\'t chose number > 100');
+  if (gridItemInOneLine > 100) {
+    alert ("Please limimt yourself with numbers <= 100");
+    reset();
+  } else {
+    makePad(gridItemInOneLine);
+    drawing();
+  }
+}
+
+
+// Drawing by mouse move
+function drawing() {
+  Array.from(document.getElementsByClassName('grid-item')).forEach(function(item) {
+    item.addEventListener('mouseenter', function(e) {
+      e.target.classList.add('drawing');
   });
 });
- 
+}
